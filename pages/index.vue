5d25349821127fbfecd66acddcc498e9ce773d7b
satoshi2372 <ape2372@gmail.comgit config --global user.email ape2372@gmail.com>
2020-11-20 17:17:27 +0900

    nuxt.jsとvuexを使用しカウンターアプリ作成

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div>
+      <Counter></Counter>
+      <hr>
+      <Counter></Counter>
+      <hr>
+      <Counter></Counter>
+      <hr>
+    </div>
+  </div>
+</template>
+
+<script>
+// nuxtでコンポーネント使用の際の記述方法
+import Counter from '~/components/Counter.vue'
+export default {
+  components: {
+    Counter
+  }
+}
+</script>
